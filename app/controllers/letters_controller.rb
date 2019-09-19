6d10dcd62c347f03f85426890558713e82fbc731
hiro <hironori.matsuse@gmail.com>
2019-09-19 13:54:25 +0900

    Add:Letter新規画面作成完了

--- app/controllers/letters_controller.rb
+++ app/controllers/letters_controller.rb
@@ -1,4 +1,12 @@
 class LettersController < ApplicationController
   def index
   end
+
+  def new
+    @letter = Letter.new
+  end
+
+  def create
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  get '/letters', to:'letters#index'
+  resources :letters
 end
